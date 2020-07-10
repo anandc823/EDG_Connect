b8f16de5eb9173de848c17daa52bc5fd1af13654
unknown <anandc823@gmail.com>
2020-07-10 17:41:30 -0400

    readded sridevi's changes

--- cluster.py
+++ cluster.py
@@ -18,45 +18,61 @@ def download_data():
         f.write(r.content)
 
 def create_meetings():
-    df = pd.read_csv(filename)
+    def create_meetings():
+    df = pd.read_excel(filename)
 
-    topics = df[response_prompt].unique()
+    topics = df['response'].unique()
 
     groups = {}
 
     for topic in topics:
-        groups[topic] = list(df[df[response_prompt]==topic][email_prompt])
+        groups[topic] = list(df[df.response==topic]["Email"])
 
     meetings = []
-    
+
     for topic in topics:
         members = groups[topic]
+        if len(members)==6:
+            print("6")   # Need to fill in this part to just create two groups of three
+        else:
+            if(len(members)) == 1:
+                print("Only 1 Member Found")
 
-        #if(len(members)) == 1:
-        #   print("Only 1 Member Found")
+            if(len(members)<=max_meeting_size):
+                cur_meeting = {}
+                cur_meeting['topic'] = topic
+                cur_meeting['members'] = members
+                meetings.append(cur_meeting)
 
-        if(len(members)<=max_meeting_size):
-            cur_meeting = {}
-            cur_meeting['topic'] = topic
-            cur_meeting['members'] = members
-            meetings.append(cur_meeting)
-        
-        else:   
-            cur_meeting = {}
-            cur_meeting['topic'] = topic
-            cur_meeting['members'] = []
-            size_counter = 0
-            meeting_counter = 0
-            overflow_index = 0
-            for member in members:
-                if meeting_counter>=(len(members)//max_meeting_size):
-                    meetings[overflow_index][members].append(member)
-                    overflow_index+=1
-                else:
+            else:
+                cur_meeting = {}
+                cur_meeting['topic'] = topic
+                cur_meeting['members'] = []
+                size_counter = 0
+                meeting_counter = 0
+                overflow_index = 0
+                for member in members:
                     size_counter+=1
                     if(size_counter>=max_meeting_size):
-                        meetings.append(cur_meeting)
+                        cur_meeting['members'].append(member)
+                        meetings.append(cur_meeting.copy())
+                        print("built 4")
+                        print(meetings)
                         size_counter = 0
+                        cur_meeting['members'] = []
+                        print(meetings)
+                    else:
+                        cur_meeting['members'].append(member)
+                        print('building block of 4',cur_meeting)
+                if len(cur_meeting['members'])==1 or len(cur_meeting['members'])==2:
+                    index = -1
+                    for extraMember in cur_meeting['members']:
+                        meetings[index]['members'].append(extraMember)
+                        index-=1
+                else:
+                    meetings.append(cur_meeting)
+
+    return meetings
 
 def getFriday():
     d = datetime.date.today()+datetime.timedelta(1)
